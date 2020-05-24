efee884f762af682992792a1e4a46a65dcb6e2b2
PabloCerero95 <56443019+PabloCerero95@users.noreply.github.com>
2020-05-25 01:41:32 +0200

    #NULLMOROROS PROYECTO FINAL#

---
+++ NullMorosos/src/test/java/com/NullMorososAPP/AppNullMorososApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NullMorososAPP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNullMorososApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
